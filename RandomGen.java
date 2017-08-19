8486d2842291864cab4b3543d51a2277709a4317
Bryce <bcrossley@unomaha.edu>
2017-08-18 23:05:37 -0500

    Test commit

--- RandomGen.java
+++ RandomGen.java
@@ -1,6 +1,6 @@
 public class RandomGen {
     public RandomGen() {
-        System.out.println("Hello World!");
+        System.out.println("Test!");
     }
 
     public static void main (String args []) {
