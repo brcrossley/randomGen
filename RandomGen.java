68059a8316204cb4a0e70f6faf3a23fc86ad76a8
Bryce <bcrossley@unomaha.edu>
2017-08-18 23:04:13 -0500

    First commit.

---
+++ RandomGen.java
@@ -0,0 +1,9 @@
+public class RandomGen {
+    public RandomGen() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main (String args []) {
+        new RandomGen();
+    }
+}
